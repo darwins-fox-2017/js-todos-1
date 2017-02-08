d5afee4395be5a11087c3ac03374fdc42e6740bd
Isumi <isumi.karina72@gmail.com>
2017-02-08 14:49:34 +0700

    Finish

--- todo.js
+++ todo.js
@@ -16,6 +16,18 @@ class TodoList {
       case 'list':
         this.list()
         break;
+      case 'add':
+        this.add(this.command.slice(1).join(' '))
+        break;
+      case 'delete':
+        this.delete(this.command.slice(1,2)[0])
+        break;
+      case 'complete':
+        this.complete(this.command.slice(1,2)[0])
+        break;
+      case 'uncomplete':
+        this.uncomplete(this.command.slice(1,2)[0])
+        break;
       default:
         console.log('Please input correct command!');
         this.help()
@@ -35,13 +47,64 @@ class TodoList {
   }
 
   list() {
-    if (this.data > 0) {
+    if (this.data.length > 0) {
       for (let i = 0; i < this.data.length; i++) {
         console.log(`${this.data[i].id}. [${this.data[i].completed ? "X" : " "}] ${this.data[i].task}`);
       }
     } else {
       console.log('-');
     }
+  }
+
+  save() {
+    json.writeFileSync('data.json', this.data)
+  }
+
+  sort() {
+    for (let i = 0; i < this.data.length; i++) {
+      this.data[i].id = i+1
+    }
+    return this.data
+  }
+
+  add(string) {
+      let formatAdd   = {"id": this.data.length+1, "task": string, "completed": false}
+      this.data.push(formatAdd)
+      this.sort()
+      this.save()
+      console.log("Success added "+string);
+  }
+
+  delete(id) {
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id == id) {
+        console.log("Record deleted "+this.data[i].task);
+        this.data.splice(i, 1)
+        break
+      }
+    }
+    this.sort()
+    this.save()
+  }
+
+  complete(id) {
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id == id) {
+        this.data[i].completed = true
+        break
+      }
+    }
+    this.save()
+  }
+
+  uncomplete(id) {
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id == id) {
+        this.data[i].completed = false
+        break
+      }
+    }
+    this.save()
   }
 }
 
