38d3be0b962bb40196e35c36ab09a860892267c6
danilags <aguspermadi94@gmail.com>
2017-02-08 17:09:15 +0700

    terbaru

--- todo.js
+++ todo.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
-let readFile = fs.readFileSync('data.json', 'utf-8') 
-// console.log(readFile)
+
 class Todo {
 	constructor(list_data) {
 		//this.data = json.readFileSync('data.json')
@@ -9,41 +7,108 @@ class Todo {
 	}
 
 	Help() {
-		// return list_data.push(this.data);
-		// let argv = process.argv
-		// let str = ""
-		// for(let i = 2; i < argv.length; i++) {
-		//   arr += argv[i] + " "
-		// }
 		switch(this.list_data[2]) {
-			case "Help" : 
-			console.log("$===================================$")
-			console.log("$ node todo.js # will can help")
-			console.log("$ node todo.js help")
-			console.log("$ node todo.js list")
-			console.log("$ node todo.js add <task_content>")
-			console.log("$ node todo.js task <task_id>")
-			console.log("$ node todo.js delete <task_id>")
-			console.log("$ node todo.js complete <task_id>")
-			console.log("$ node todo.js uncomplete <task_id>")
-			break;
-			case "List" :
-			console.log("Hello")
+			case "help" : 
+				console.log("$===================================$")
+				console.log("$ node todo.js # will can help")
+				console.log("$ node todo.js help")
+				console.log("$ node todo.js list")
+				console.log("$ node todo.js add <task_content>")
+				console.log("$ node todo.js task <task_id>")
+				console.log("$ node todo.js delete <task_id>")
+				console.log("$ node todo.js complete <task_id>")
+				console.log("$ node todo.js uncomplete <task_id>")
+				break;
+			case "list" :
+				this.listData()
+				break;
+			case "add" :
+				this.addData()
+				break;
+			case "task" : 
+				this.task()
+				break;
+			case "delete" :
+				this.delete()
+				break;
+			case "complete" :
+				this.complete()
+				break;
+			case "uncomplete" :
+				this.uncompleted()
+				break;
+
 		}	
 		// console.log(convert(arr))
 	}
 
-	addData() {
-		for(let i = 0; i < readFile.length; i++) {
-			if(readFile[2] === "add") {
-				let argvAdd = process.argv
+	listData() {
+		console.log("====== Cek list data ======")
+		for(let i = 0; i < readDataFile.length; i++) {
+			if(readDataFile[i].completed === true) {
+				console.log(`${i + 1}. [X] ${readDataFile[i].task}`)
+			} else {
+				console.log(`${i + 1}. [ ] ${readDataFile[i].task}`)
 			}
 		}
 		
 	}
 
+	addData() {
+		console.log("====== Add data record ======")
+		let addthis = testargv.slice(3).join(" ")
+
+		readDataFile.push({
+			'id' : readDataFile.length+1,
+			'task' : addthis,
+			'completed' : false
+		})
+		console.log(`${addthis} ditambah`)
+
+
+		fs.writeFileSync('data.json', JSON.stringify(readDataFile, false, 3), 'utf-8')
+
+	}
+
+	task() {
+		for(let i = 0; i < readDataFile.length; i++) {
+			if(readDataFile[i].id === readDataFile[3].id) {
+				console.log(`readDataFile[i].id`)
+			}
+		}
+		fs.writeFileSync('data.json', JSON.stringify(readDataFile, false, 3), 'utf-8')
+
+	}
+
+	delete() {
+
+		delete readDataFile[testargv[3] -1]
+		readDataFile.splice(testargv[3]-1, 1)
+		console.log("Data di hapus")
+		fs.writeFileSync('data.json', JSON.stringify(readDataFile, false, 3), 'utf-8')
+	}
+
+	complete() {
+		console.log("Task yang ke - " + this.list_data[3])
+				
+		readDataFile[testargv[3] - 1].completed = true
+		console.log(`${readDataFile[testargv[3] - 1].task} Completed!`)	
+		fs.writeFileSync('data.json', JSON.stringify(readDataFile, false, 3), 'utf-8')
+	}
+
+	uncompleted() {
+		// let uncompleted = testargv.slice(1).join(" ")
+		readDataFile[testargv[3] - 1].completed = false
+
+		console.log(`${readDataFile[testargv[3] - 1].task} uncompleted`)
+		fs.writeFileSync('data.json', JSON.stringify(readDataFile, false, 3), 'utf-8')
+	}	
 
 }
+const fs = require('fs')
+
+let readDataFile = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
 let testargv = process.argv
+
 let todo_test = new Todo(testargv)
 todo_test.Help()
