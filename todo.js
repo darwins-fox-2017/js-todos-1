7ab3e08d7db84b75c9474a21e89a47c187b45ff0
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-08 14:59:33 +0700

    Done

--- todo.js
+++ todo.js
@@ -53,6 +53,7 @@ class listToDo {
     console.log(`$ node todo.js list`);
     console.log(`$ node todo.js add <task_content>`);
     console.log(`$ node todo.js delete <task_id>`);
+    console.log(`$ node todo.js task <task_id>`);
     console.log(`$ node todo.js complete <task_id>`);
     console.log(`$ node todo.js delete <task_id>`);
     console.log(`$ node todo.js uncomplete <task_id>`);
